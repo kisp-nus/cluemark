176c7cbb6a6052320b70a19bf7b68deafc4a6784
Kareem Shehata <kareem@shehata.ca>
2024-10-19 08:38:10 +0000

    Brought in the "exact" inverse DPM. Seems to improve MSE on reconstructing latents from 0.1 ish to 0.01 ish. Still need to see what effect this will have on robustness.

--- pipeline_runner.py
+++ pipeline_runner.py
@@ -2,6 +2,7 @@ import torch
 from torchvision import transforms
 from inverse_stable_diffusion import InversableStableDiffusionPipeline
 from diffusers import DPMSolverMultistepScheduler
+from config import get_dtype
 import numpy as np
 import random
 
@@ -30,21 +31,27 @@ class PipelineRunner:
     def __init__(self, conf) -> None:
         self.model_id = conf.model_id
         self.device = conf.device
-        scheduler = DPMSolverMultistepScheduler.from_pretrained(self.model_id, subfolder='scheduler')
+        scheduler = DPMSolverMultistepScheduler(
+            beta_end=0.012,
+            beta_schedule='scaled_linear',
+            beta_start=0.00085,
+            num_train_timesteps=1000,
+            prediction_type="epsilon",
+            steps_offset=1, 
+            trained_betas=None,
+            solver_order=conf.solver_order,
+        )
         self._pipe = InversableStableDiffusionPipeline.from_pretrained(
             self.model_id,
             scheduler=scheduler,
-            torch_dtype=torch.float16,
-            revision='fp16',
+            torch_dtype=get_dtype(conf.dtype),
             )
         self._pipe = self._pipe.to(self.device)
         self.guidance_scale = conf.guidance_scale
         self.num_inference_steps = conf.num_inference_steps
         self.image_width = conf.image_width
         self.image_height = conf.get("image_height", self.image_width)
-        # save empty text embeddings for inverting model,
-        # since we assume that at retrieval we don't have the promp
-        self.text_embeddings = self._pipe.get_text_embedding("")
+        self.inv_order = conf.get("inv_order", conf.solver_order)
 
     def get_random_latents(self, seed = None):
         if seed: set_random_seed(seed)
@@ -59,14 +66,23 @@ class PipelineRunner:
             height=self.image_width,
             width=self.image_height,
             latents=latents,
-        ).images[0]
+        )[0].images[0]
+
+    def get_text_embeddings(self, prompt):
+        text_embeddings_tuple = self._pipe.encode_prompt(
+            prompt, self.device, 1, self.guidance_scale > 1.0, None
+        )
+        return torch.cat([text_embeddings_tuple[1], text_embeddings_tuple[0]])
     
-    def invert_image(self, img):
-        trans_img = transform_img(img).unsqueeze(0).to(self.text_embeddings.dtype).to(self.device)
-        image_latents = self._pipe.get_image_latents(trans_img, sample=False)
+    def invert_image(self, img, prompt=""):
+        text_embeddings = self.get_text_embeddings(prompt)
+        trans_img = transform_img(img).unsqueeze(0).to(text_embeddings.dtype).to(self.device)
+        image_latents = self._pipe.decoder_inv(trans_img)
         return self._pipe.forward_diffusion(
             latents=image_latents,
-            text_embeddings=self.text_embeddings,
-            guidance_scale=1,
+            text_embeddings=text_embeddings,
+            guidance_scale=self.guidance_scale,
             num_inference_steps=self.num_inference_steps,
+            inverse_opt=(self.inv_order != 0),
+            inv_order=self.inv_order
         )
---
+++ test_inv_dpm.py
@@ -0,0 +1,59 @@
+import PIL
+from pipeline_runner import *
+from clwe_watermark import *
+from image_filters import *
+from config import *
+from tqdm import trange
+from datasets import load_dataset
+import os
+
+def get_dataset(dataset_name):
+    if 'laion' in dataset_name:
+        dataset = load_dataset(dataset_name)['train']
+        prompt_key = 'TEXT'
+    elif 'coco' in dataset_name:
+        with open('fid_outputs/coco/meta_data.json') as f:
+            dataset = json.load(f)
+            dataset = dataset['annotations']
+            prompt_key = 'caption'
+    else:
+        dataset = load_dataset(dataset_name)['test']
+        prompt_key = 'Prompt'
+
+    return dataset, prompt_key
+
+def calc_mse(l1, l2):
+    err = l2 - l1
+    return torch.mean(err * err).item()
+
+conf = get_config()
+print("Config:", OmegaConf.to_container(conf, resolve=True, throw_on_missing=False))
+
+dataset, prompt_key = get_dataset(conf.get("dataset", "Gustavosta/Stable-Diffusion-Prompts"))
+runner = PipelineRunner(get_section(conf, "pipeline"))
+wm = get_watermark_from_conf(get_section(conf, "watermark"))
+
+
+for i in trange(conf.start, conf.end):
+    seed = i + conf.seed
+    current_prompt = dataset[i][prompt_key]
+    reverse_prompt = current_prompt if conf.get("reverse_with_prompt", False) else ""
+
+    ### Generation
+    no_wm_latents = runner.get_random_latents(seed)
+    no_wm_image = runner.sample_image(current_prompt, no_wm_latents)
+    no_wm_reversed_latents = runner.invert_image(no_wm_image, reverse_prompt)
+    results = [i, calc_mse(no_wm_latents, no_wm_reversed_latents),
+              wm.check_watermark(no_wm_latents), wm.check_watermark(no_wm_reversed_latents)]
+
+    if wm:
+        wm_latents = wm.inject_watermark(no_wm_latents)
+        wm_image = runner.sample_image(current_prompt, wm_latents)
+        wm_reversed_latents = runner.invert_image(wm_image, reverse_prompt)
+        results += [calc_mse(wm_latents, wm_reversed_latents),
+                wm.check_watermark(wm_latents), wm.check_watermark(wm_reversed_latents)]
+
+    print()
+    print(",".join([str(x) for x in results]))
+    print()
+    print()
