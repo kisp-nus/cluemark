76da4f793ffff64ab6b3a1091c82e0c31887fccb
Kareem Shehata <kareem@shehata.ca>
2024-10-19 09:03:18 +0000

    Improvements to the runner script.

--- run_clwe_checks.sh
+++ run_clwe_checks.sh
@@ -2,24 +2,73 @@
 
 START=0
 END=10
+CHECK_WM=true
+GEN_IMG=true
+
+function usage {
+    echo "Usage: $(basename $0) [options] config [device]"
+    echo "Options:"
+    echo "    -b Base (no watermark)"
+    echo "    -s ## Start prompt number"
+    echo "    -e ## End prompt number"
+    echo "    -n No image generation, just check watermarks"
+    echo "    -h help"
+}
+
+while getopts 'bs:e:nh' opt; do
+  case "$opt" in
+    b)
+      CHECK_WM=false
+      ;;
+
+    s)
+      START=$OPTARG
+      ;;
+
+    e)
+      END=$OPTARG
+      ;;
+
+    n)
+      GEN_IMG=false
+      ;;
+   
+    ?|h)
+      usage
+      exit 1
+      ;;
+  esac
+done
+shift "$(($OPTIND -1))"
+
+if [ $# -lt 1 ]; then
+    usage
+    exit 1
+fi
 
 CONFIG_FILE=config/$1.yaml
 
+if [ $# -ge 2 ]; then
+  DEVICE=$2
+elif [ "$DEVICE" == "" ]; then
+  DEVICE="cuda:0"
+fi
+
 if [ ! -r "$CONFIG_FILE" ]; then
-    echo "Usage $0 [conf name]"
-    exit 1
+    echo "Cannot read config file $CONFIG_FILE"
+    exit 2
 fi
 
 if [ ! -d results ]; then
     mkdir results
 fi
 
-if [ "$2" != "nogen" ]; then
+if $GEN_IMG; then
     echo Generating images for $1
     python generate_images.py $CONFIG_FILE device=$DEVICE start=$START end=$END
 fi
 
-if [ "$1" != "base" ]; then
+if $CHECK_WM; then
     echo Checking watermarks for $1
     python check_watermark.py $CONFIG_FILE device=$DEVICE start=$START end=$END no_wm_path=images/no_wm | tee results/${1}.txt
     cat results/${1}.txt
