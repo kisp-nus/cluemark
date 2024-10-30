9732145c99070c794c53a6a6bd1d7286a150e50f
Kareem Shehata <kareem@shehata.ca>
2024-10-30 10:27:57 +0000

    Make steg attack compatible with AUC script.

--- steg_from_images.py
+++ steg_from_images.py
@@ -5,7 +5,7 @@ from util.image_filters import *
 from util.config import *
 from util.utils import *
 from tqdm import trange
-import os
+import os, sys
 
 conf = get_config()
 print("Config:", OmegaConf.to_container(conf, resolve=True, throw_on_missing=False))
@@ -20,7 +20,7 @@ wm_path = conf.output_path
 output_path = conf.output_path
 os.makedirs(output_path, exist_ok=True)
 
-print("Reading images")
+print("Reading images", file=sys.stderr)
 wm_avg = np.zeros((runner.image_height, runner.image_width, 3), dtype=np.float64)
 count = 0
 for i in trange(conf.start, conf.end):
@@ -32,13 +32,14 @@ for i in trange(conf.start, conf.end):
     wm_avg += np.asarray(wm_img).astype(np.float64) - np.asarray(no_wm_img).astype(np.float64)
     count += 1
 
-print("Averaging")
+print("Averaging", file=sys.stderr)
 # average all of the watermarked images
 wm_avg /= count
 wm_avg_img = PIL.Image.fromarray(np.uint8(np.clip(wm_avg + 127, 0, 255)))
 wm_avg_img.save(os.path.join(output_path, "wm_avg.png"))
 
-print("Testing steg removal attack")
+print("Testing steg removal attack", file=sys.stderr)
+print("i,steg_no_wm,steg_wm")
 for i in trange(conf.start, conf.end):
     no_wm_img, no_wm_meta = load_image(no_wm_path, i)
     wm_img, wm_meta = load_image(wm_path, i)
