abbaa46afaf8e780773b1ab17f3e8c44f4da6032
Kareem Shehata <kareem@shehata.ca>
2025-08-25 00:56:44 +0000

    Update latent calcs for filters.

--- calc_latents.py
+++ calc_latents.py
@@ -1,6 +1,7 @@
 from pipeline_runner import get_pipline_runner
 from util.config import *
 from util.utils import *
+from util.image_filters import get_filters
 from tqdm import trange
 import os
 
@@ -11,11 +12,19 @@ runner = get_pipline_runner(get_section(conf, "pipeline"))
 
 output_path = conf.output_path
 
+filters = get_filters(get_section(conf, "filters"))
+
 for i in trange(conf.start, conf.end):
     img, meta = load_image(output_path, i)
-    latents = runner.invert_image(img)
+    results = {
+        "meta": meta,
+        "og_latents": meta["latents"],
+        "rev_latents": runner.invert_image(img)
+    }
 
-    torch.save({
-        "orig_latents": meta["latents"],
-        "rev_latents": latents
-    }, os.path.join(output_path, f"latents_{i:05}.pt"))
+    for name, f in filters:
+        # Refresh the pipeline each time, because for some reason it isn't repeatable.
+        runner = get_pipline_runner(get_section(conf, "pipeline"))
+        results[name] = runner.invert_image(f(img))
+
+    torch.save(results, os.path.join(output_path, f"latents_{i:05}.pt"))
